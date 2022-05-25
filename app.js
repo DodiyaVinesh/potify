dab68d879dfb9e09e8041dfdc746c2b2fae11068
Vinesh Dodiya <dodiyavinesh352@gmail.com>
2022-05-25 17:54:28 +0530

    updated hosting

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-if (process.env.NODE_ENV == "production") {
-  // app.use(express.static("portfolio"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/dist/client", "index.html"));
-  });
-}
+app.use(express.static(__dirname + "/client/dist/client"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/client/dist/client/index.html"));
+});
+
 app.listen(port, () => {
   console.log("Listning to port " + port);
 });
